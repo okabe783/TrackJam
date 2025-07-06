edfe64af6ff8646d43185c8d4dc7cbe6d4d706cc
kenta17291113 <kenta17291113@gmail.com>
2025-07-06 18:24:55 +0900

    プレイヤーの移動と、カメラ追従、HPの減らす処理(１づつ)

---
+++ Assets/Player/PlayerStatusData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Data/Create StatusData")]
+public class StatusData : ScriptableObject
+{
+    public float _hp;//HP
+    public float _moveSpeed; //移動速度
+}

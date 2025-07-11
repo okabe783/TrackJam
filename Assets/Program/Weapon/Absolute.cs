679d625ace16b338371ebcf37752309be47a16b7
junyoko1 <junyoko1@outlook.jp>
2025-07-12 00:57:13 +0900

    ATフィールドの追加　エネミータイマーのちょっとだけ進めた

---
+++ Assets/Program/Weapon/Absolute.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Absolute : MonoBehaviour
+{
+    [SerializeField] float attackRange = 1.5f;
+    [SerializeField] int attackDamage = 2;
+    [SerializeField] LayerMask enemyLayer;
+    private float cooldown = 3f;
+    float count = 2;
+    Animator animator;
+    public Transform attackPoint;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+
+
+        if (count > cooldown)
+        {
+            count = 0;
+            attack();
+        }
+
+
+    }
+
+    void attack()
+    {
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+
+
+        }
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
